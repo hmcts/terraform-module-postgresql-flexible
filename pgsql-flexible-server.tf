593620eb954792f23ae338bc72f638b6bbde1b67
Chirag Pareek <111435448+cpareek@users.noreply.github.com>
2023-01-26 11:56:59 +0000

    Revert "Dtspo 12269 multiple admin read groups (#25)"
    This reverts commit f15c39b95f3d3fac6eb8ca310e47860bc2c10ce0.

--- pgsql-flexible-server.tf
+++ pgsql-flexible-server.tf
@@ -10,9 +10,8 @@ locals {
   private_dns_zone_id = "/subscriptions/1baf5470-1c3e-40d3-a6f7-74bfbce4b348/resourceGroups/core-infra-intsvc-rg/providers/Microsoft.Network/privateDnsZones/private.postgres.database.azure.com"
 
   is_prod        = length(regexall(".*(prod).*", var.env)) > 0
-  admin_group    = local.is_prod ? (var.add_multiple_admin_groups ? split(",", join(",", ["DTS Platform Operations SC", var.additional_admin_groups])) : split(",","DTS Platform Operations SC")) : (var.add_multiple_admin_groups ? split(",", join(",", ["DTS Platform Operations", var.additional_admin_groups])) : split(",","DTS Platform Operations"))
-  db_reader_user = local.is_prod ? (var.add_multiple_readonly_groups ? split(",", join(",", ["DTS JIT Access ${var.product} DB Reader SC", var.additional_readonly_groups])) : split(",","DTS JIT Access ${var.product} DB Reader SC")) : (var.add_multiple_readonly_groups ? split(",", join(",", ["DTS ${upper(var.business_area)} DB Access Reader", var.additional_readonly_groups])) : split(",","DTS ${upper(var.business_area)} DB Access Reader"))
-
+  admin_group    = local.is_prod ? "DTS Platform Operations SC" : "DTS Platform Operations"
+  db_reader_user = local.is_prod ? "DTS JIT Access ${var.product} DB Reader SC" : "DTS ${upper(var.business_area)} DB Access Reader"
 }
 
 data "azurerm_subnet" "pg_subnet" {
@@ -26,8 +25,7 @@ data "azurerm_subnet" "pg_subnet" {
 data "azurerm_client_config" "current" {}
 
 data "azuread_group" "db_admin" {
-  count            = length(local.admin_group)
-  display_name     = local.admin_group[count.index]
+  display_name     = local.admin_group
   security_enabled = true
 }
 
@@ -104,12 +102,11 @@ resource "azurerm_postgresql_flexible_server_configuration" "pgsql_server_config
 }
 
 resource "azurerm_postgresql_flexible_server_active_directory_administrator" "pgsql_adadmin" {
-  count               = length(local.admin_group)
   server_name         = azurerm_postgresql_flexible_server.pgsql_server.name
   resource_group_name = azurerm_postgresql_flexible_server.pgsql_server.resource_group_name
   tenant_id           = data.azurerm_client_config.current.tenant_id
-  object_id           = data.azuread_group.db_admin[count.index].object_id
-  principal_name      = local.admin_group[count.index]
+  object_id           = data.azuread_group.db_admin.object_id
+  principal_name      = local.admin_group
   principal_type      = "Group"
   depends_on = [
     azurerm_postgresql_flexible_server.pgsql_server
@@ -130,12 +127,12 @@ resource "azurerm_postgresql_flexible_server_active_directory_administrator" "pg
 }
 
 resource "null_resource" "set-user-permissions-additionaldbs" {
-  count = var.enable_read_only_group_access ? length(local.db_reader_user) : 0
+  count = var.enable_read_only_group_access ? 1 : 0
 
   triggers = {
     script_hash    = filesha256("${path.module}/set-postgres-permissions.bash")
     name           = local.name
-    db_reader_user = local.db_reader_user[count.index]
+    db_reader_user = local.db_reader_user
   }
 
   provisioner "local-exec" {
@@ -144,7 +141,7 @@ resource "null_resource" "set-user-permissions-additionaldbs" {
     environment = {
       DB_HOST_NAME   = azurerm_postgresql_flexible_server.pgsql_server.fqdn
       DB_USER        = "${data.azuread_service_principal.mi_name[0].display_name}"
-      DB_READER_USER = local.db_reader_user[count.index]
+      DB_READER_USER = local.db_reader_user
     }
   }
   depends_on = [
