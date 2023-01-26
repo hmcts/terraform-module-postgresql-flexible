da67f94cccf075b75fcd4770eb5d66afa26a3d6e
Chirag Pareek <chirag.pareek@justice.gov.uk>
2023-01-26 11:25:50 +0000

    fixed issue with tolist

--- pgsql-flexible-server.tf
+++ pgsql-flexible-server.tf
@@ -10,7 +10,7 @@ locals {
   private_dns_zone_id = "/subscriptions/1baf5470-1c3e-40d3-a6f7-74bfbce4b348/resourceGroups/core-infra-intsvc-rg/providers/Microsoft.Network/privateDnsZones/private.postgres.database.azure.com"
 
   is_prod        = length(regexall(".*(prod).*", var.env)) > 0
-  admin_group    = local.is_prod ? (var.add_multiple_admin_groups ? split(",", join(",", ["DTS Platform Operations SC", var.additional_admin_groups])) : tolist("DTS Platform Operations SC")) : (var.add_multiple_admin_groups ? split(",", join(",", ["DTS Platform Operations", var.additional_admin_groups])) : tolist("DTS Platform Operations"))
+  admin_group    = local.is_prod ? (var.add_multiple_admin_groups ? split(",", join(",", ["DTS Platform Operations SC", var.additional_admin_groups])) : split(",","DTS Platform Operations SC")) : (var.add_multiple_admin_groups ? split(",", join(",", ["DTS Platform Operations", var.additional_admin_groups])) : split(",","DTS Platform Operations"))
   db_reader_user = local.is_prod ? (var.add_multiple_readonly_groups ? split(",", join(",", ["DTS JIT Access ${var.product} DB Reader SC", var.additional_readonly_groups])) : split(",","DTS JIT Access ${var.product} DB Reader SC")) : (var.add_multiple_readonly_groups ? split(",", join(",", ["DTS ${upper(var.business_area)} DB Access Reader", var.additional_readonly_groups])) : split(",","DTS ${upper(var.business_area)} DB Access Reader"))
 
 }
