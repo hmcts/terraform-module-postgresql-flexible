4292266653951ac5892efedde5f5a093f1e1adcf
Thomas Thornton <>
2023-03-29 10:45:11 +0100

    switch to provider provided by team

--- init.tf
+++ init.tf
@@ -1,19 +1,13 @@
 terraform {
   required_providers {
     azurerm = {
-      source  = "hashicorp/azurerm"
-      version = ">= 3.7.0"
+      source                = "hashicorp/azurerm"
+      version               = ">= 3.7.0"
+      configuration_aliases = [azurerm.aks_subscription]
     }
     random = {
       source  = "hashicorp/random"
       version = ">= 3.2.0"
     }
   }
-}
-
-provider "azurerm" {
-  alias                      = "subscription"
-  skip_provider_registration = "true"
-  features {}
-  subscription_id = var.business_area == "sds" ? local.sds_subscriptions[var.env].subscription : local.cft_subscriptions[var.env].subscription
 }
