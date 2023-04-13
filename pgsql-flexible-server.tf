9d527e4ba319a99af347f777d4da507c989a0d10
Tyler-35 <115563258+Tyler-35@users.noreply.github.com>
2023-04-13 10:09:49 +0100

    Update pgsql-flexible-server.tf

--- pgsql-flexible-server.tf
+++ pgsql-flexible-server.tf
@@ -4,8 +4,9 @@ locals {
   server_name            = "${local.name}-${var.env}"
   postgresql_rg_name     = var.resource_group_name == null ? azurerm_resource_group.rg[0].name : var.resource_group_name
   postgresql_rg_location = var.resource_group_name == null ? azurerm_resource_group.rg[0].location : var.location
-  vnet_rg_name           = var.business_area == "sds" ? "ss-${var.env}-network-rg" : "cft-${var.env}-network-rg"
-  vnet_name              = var.business_area == "sds" ? "ss-${var.env}-vnet" : "cft-${var.env}-vnet"
+  env                    = var.env == "sandbox" ? "sbox" : var.env
+  vnet_rg_name           = var.business_area == "sds" ? "ss-${var.env}-network-rg" : "cft-${local.env}-network-rg"
+  vnet_name              = var.business_area == "sds" ? "ss-${var.env}-vnet" : "cft-${local.env}-vnet"
 
   private_dns_zone_id = "/subscriptions/1baf5470-1c3e-40d3-a6f7-74bfbce4b348/resourceGroups/core-infra-intsvc-rg/providers/Microsoft.Network/privateDnsZones/private.postgres.database.azure.com"
 
