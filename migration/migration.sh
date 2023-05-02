34b197bdbef61363225d379d8bb9916dd6fd78fb
matt.slater <matt.slater@justice.gov.uk>
2023-05-02 16:09:39 +0100

    DTSPO-13593 - fix json file

--- migration/migration.sh
+++ migration/migration.sh
@@ -6,12 +6,12 @@ single_server_admin_password=$(az keyvault secret show --vault-name "${single_se
 flexible_server_admin_password=$(az keyvault secret show --vault-name "${flexible_server_kv_name}" --name "${flexible_server_secret_name}" --subscription "${kv_subscription}" --query value -o tsv)
 
 # Command to update json file
-jq ".properties |
-  .OverwriteDBsInTarget = \"${overwrite_target_dbs}\" |
-  .SourceDBServerResourceId = \"${single_server_resource_id}\" |
-  .DBsToMigrate = ${dbs_to_migrate} |
-  .SecretParameters.AdminCredentials.SourceServerPassword = \"${single_server_admin_password}\" |
-  .SecretParameters.AdminCredentials.TargetServerPassword = \"${flexible_server_admin_password}\" " ${template_properties_file} > ${properties_file}
+jq ". |
+  .properties.OverwriteDBsInTarget = \"${overwrite_target_dbs}\" |
+  .properties.SourceDBServerResourceId = \"${single_server_resource_id}\" |
+  .properties.DBsToMigrate = ${dbs_to_migrate} |
+  .properties.SecretParameters.AdminCredentials.SourceServerPassword = \"${single_server_admin_password}\" |
+  .properties.SecretParameters.AdminCredentials.TargetServerPassword = \"${flexible_server_admin_password}\" " ${template_properties_file} > ${properties_file}
 
 echo "###################################"
 cat ${properties_file}
@@ -30,3 +30,12 @@ az postgres flexible-server migration show \
     --resource-group "${resource_group}"\
     --name "${flexible_server_name}" \
     --migration-name "${migration_name}"
+
+
+
+az postgres flexible-server migration create \
+    --subscription "DCD-CFTAPPS-SBOX" \
+    --resource-group "fliexible-migration-test"\
+    --name "ccd-data-store-api-flexible-migration-test" \
+    --migration-name "migration1" \
+    --properties config.json
