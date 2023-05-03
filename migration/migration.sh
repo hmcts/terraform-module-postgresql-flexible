2dc898d8b06253b8d5a4bee7232cf75617f0ceac
matt.slater <matt.slater@justice.gov.uk>
2023-05-03 10:27:21 +0100

    DTSPO-13593 - echo state

--- migration/migration.sh
+++ migration/migration.sh
@@ -5,6 +5,9 @@ properties_file="migration/migration-config.json"
 single_server_admin_password=$(az keyvault secret show --vault-name "${single_server_kv_name}" --name "${single_server_secret_name}" --subscription "${kv_subscription}" --query value -o tsv)
 flexible_server_admin_password=$(az keyvault secret show --vault-name "${flexible_server_kv_name}" --name "${flexible_server_secret_name}" --subscription "${kv_subscription}" --query value -o tsv)
 
+
+# TODO check migration name doesn't exist
+
 # Command to update json file
 jq ". |
   .properties.OverwriteDBsInTarget = \"${overwrite_target_dbs}\" |
@@ -21,8 +24,12 @@ az postgres flexible-server migration create \
     --migration-name "${migration_name}" \
     --properties ${properties_file}
 
-az postgres flexible-server migration show \
+state=$(az postgres flexible-server migration show \
     --subscription "${subscription}" \
     --resource-group "${resource_group}"\
     --name "${flexible_server_name}" \
-    --migration-name "${migration_name}"
+    --migration-name "${migration_name}" \
+    --query ".properties.currentStatus.state")
+
+echo "${state}"
+
