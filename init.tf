907e12e62b9c1d74b646ebb9aa7a0b14b9a0565a
nicholas chang <nicholasc001@hotmail.com>
2024-10-14 16:37:40 +0100

    edit azurerm version

--- init.tf
+++ init.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source                = "hashicorp/azurerm"
-      version               = ">= 3.7.0"
+      version               = ">= 4.5.0"
       configuration_aliases = [azurerm.postgres_network]
     }
     random = {
