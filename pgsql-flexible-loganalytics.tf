1041fb9bedea325048b61a1cbc3b2e03f63ecbac
Daniel Wilson <daniel.wilson@kainos.com>
2024-10-17 15:04:55 +0100

    add qpi functionality

---
+++ pgsql-flexible-loganalytics.tf
@@ -0,0 +1,24 @@
+resource "azurerm_log_analytics_workspace" "pgsql_log_analytics_workspace" {
+  count               = var.enable_qpi ? 1 : 0
+  name                = "${server_name}-workspace"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
+}
+
+resource "azurerm_monitor_diagnostic_setting" "pgsql_diag" {
+  count                      = var.enable_qpi ? 1 : 0
+  name                       = "${server_name}-to-log-analytics"
+  target_resource_id         = azurerm_postgresql_flexible_server.pgsql_server.id
+  log_analytics_workspace_id = azurerm_log_analytics_workspace.pgsql_log_analytics_workspace.id
+
+  enabled_log {
+    category_group = "PostgreSQLLogs"
+  }
+
+  depends_on = [
+    azurerm_postgresql_flexible_server.pgsql_server,
+    azurerm_log_analytics_workspace.pgsql_log_analytics_workspace
+  ]
+}
