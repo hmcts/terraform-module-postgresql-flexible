48064861e48c4c1af4394126102f951301d0c443
Daniel Wilson <daniel.wilson@kainos.com>
2024-10-17 15:28:01 +0100

    fix local ref

--- pgsql-flexible-loganalytics.tf
+++ pgsql-flexible-loganalytics.tf
@@ -1,6 +1,6 @@
 resource "azurerm_log_analytics_workspace" "pgsql_log_analytics_workspace" {
   count               = var.enable_qpi ? 1 : 0
-  name                = "${server_name}-workspace"
+  name                = "${local.server_name}-workspace"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
   sku                 = "PerGB2018"
@@ -9,7 +9,7 @@ resource "azurerm_log_analytics_workspace" "pgsql_log_analytics_workspace" {
 
 resource "azurerm_monitor_diagnostic_setting" "pgsql_diag" {
   count                      = var.enable_qpi ? 1 : 0
-  name                       = "${server_name}-to-log-analytics"
+  name                       = "${local.server_name}-to-log-analytics"
   target_resource_id         = azurerm_postgresql_flexible_server.pgsql_server.id
   log_analytics_workspace_id = azurerm_log_analytics_workspace.pgsql_log_analytics_workspace.id
 
