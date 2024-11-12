8c2283f771851c47e20fc8c6784e9af0219193dd
Kiran.Yenigala <kiran.yenigala@hmcts.net>
2024-11-12 10:24:21 +0000

    CME-120: remove 80 from alert name

--- alerts.tf
+++ alerts.tf
@@ -1,5 +1,5 @@
 resource "azurerm_monitor_metric_alert" "db_alert_cpu" {
-  name                = "db_cpu_percent_80_${local.server_name}"
+  name                = "db_cpu_percent_${local.server_name}"
   resource_group_name = local.postgresql_rg_name
   scopes              = [azurerm_postgresql_flexible_server.pgsql_server.id]
   description         = "Whenever the cpu utilization is greater than 80"
@@ -22,7 +22,7 @@ resource "azurerm_monitor_metric_alert" "db_alert_cpu" {
 }
 
 resource "azurerm_monitor_metric_alert" "db_alert_memory" {
-  name                = "db_memory_percent_80_${local.server_name}"
+  name                = "db_memory_percent_${local.server_name}"
   resource_group_name = local.postgresql_rg_name
   scopes              = [azurerm_postgresql_flexible_server.pgsql_server.id]
   description         = "Whenever the memory utilization is greater than 80"
@@ -45,8 +45,7 @@ resource "azurerm_monitor_metric_alert" "db_alert_memory" {
 }
 
 resource "azurerm_monitor_metric_alert" "db_alert_storage_utilization" {
-  name = "db_storage_utilization_80_${local.server_name}"
-
+  name = "db_storage_utilization_${local.server_name}"
   resource_group_name = local.postgresql_rg_name
   scopes              = [azurerm_postgresql_flexible_server.pgsql_server.id]
   description         = "Whenever the storage utilization is greater than 80"
