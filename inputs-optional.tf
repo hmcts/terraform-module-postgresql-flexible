fe720798476cc5ab169c08179c32c3fd2124c0f6
Kiran.Yenigala <kiran.yenigala@hmcts.net>
2024-12-06 12:18:12 +0000

    CME-120: change window size to 15 mins

--- inputs-optional.tf
+++ inputs-optional.tf
@@ -240,7 +240,7 @@ variable "alert_frequency" {
 variable "alert_window_size" {
   description = "The period over which the metric is evaluated."
   type        = string
-  default     = "PT30D"
+  default     = "PT15M"
 }
 
 variable "email_address_key" {
