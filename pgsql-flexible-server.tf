818db6e5f2b10354f104360c3e7792c6a9001cb9
anne-moore <Anne.Moore@justice.gov.uk>
2025-04-28 11:28:21 +0100

    Updating Postgres admin groups to new groups

--- pgsql-flexible-server.tf
+++ pgsql-flexible-server.tf
@@ -13,7 +13,7 @@ locals {
 
   is_prod = length(regexall(".*(prod).*", var.env)) > 0
 
-  admin_group    = local.is_prod ? "DTS Platform Operations SC" : "DTS Platform Operations"
+  admin_group    = local.is_prod ? "PlatOps PostgreSQL Admin Access SC" : "PlatOps PostgreSQL Admin Access"
   db_reader_user = local.is_prod ? "DTS JIT Access ${var.product} DB Reader SC" : "DTS ${upper(var.business_area)} DB Access Reader"
 
 
