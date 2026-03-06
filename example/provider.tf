730e38ac7a29527f8babd152476ff8845f01f1b7
Rees Pozzi <reespozzi@icloud.com>
2026-03-06 13:14:49 +0000

    Reverting

--- example/provider.tf
+++ example/provider.tf
@@ -8,10 +8,3 @@ provider "azurerm" {
   alias                      = "postgres_network"
   subscription_id            = var.aks_subscription_id
 }
-
-provider "azurerm" {
-  features {}
-  skip_provider_registration = true
-  alias                      = "backup"
-  subscription_id            = var.backup_subscription_id
-}
--- example/variables.tf
+++ example/variables.tf
@@ -5,7 +5,3 @@ variable "env" {
 variable "aks_subscription_id" {
   default = "3eec5bde-7feb-4566-bfb6-805df6e10b90"
 }
-
-variable "backup_subscription_id" {
-  default = "8999dec3-0104-4a27-94ee-6588559729d1"
-}
