93a20f157b2771bd9a52384c4962274c61c07c08
Rees Pozzi <rees.pozzi@justice.gov.uk>
2026-03-06 11:56:47 +0000

    Switch to use of data sources (#102)
    * Switch to use of data sources
    
    * terraform-docs: automated action
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- backup.tf
+++ backup.tf
@@ -1,3 +1,16 @@
+# Data sources for backup vault and policy
+data "azurerm_data_protection_backup_vault" "main" {
+  count               = local.enable_backup_enrollment ? 1 : 0
+  name                = var.backup_vault_name
+  resource_group_name = var.backup_vault_resource_group_name
+}
+
+data "azurerm_data_protection_backup_policy_postgresql_flexible_server" "main" {
+  count     = local.enable_backup_enrollment ? 1 : 0
+  name      = var.backup_policy_name
+  vault_id  = data.azurerm_data_protection_backup_vault.main[0].id
+}
+
 locals {
   # Enrollment requires: criticality >= 4
   enable_backup_enrollment = var.service_criticality >= 4
@@ -8,7 +21,7 @@ resource "azurerm_role_assignment" "backup_vault_rg_reader" {
   count                = local.enable_backup_enrollment && var.manage_reader_role_on_rg ? 1 : 0
   scope                = "/subscriptions/${data.azurerm_client_config.current.subscription_id}/resourceGroups/${local.postgresql_rg_name}"
   role_definition_name = "Reader"
-  principal_id         = var.backup_vault_principal_id
+  principal_id         = data.azurerm_data_protection_backup_vault.main[0].identity[0].principal_id
 
   lifecycle {
     create_before_destroy = true
@@ -20,7 +33,7 @@ resource "azurerm_role_assignment" "backup_vault_postgres_ltr" {
   count                = local.enable_backup_enrollment ? 1 : 0
   scope                = azurerm_postgresql_flexible_server.pgsql_server.id
   role_definition_name = "PostgreSQL Flexible Server Long Term Retention Backup Role"
-  principal_id         = var.backup_vault_principal_id
+  principal_id         = data.azurerm_data_protection_backup_vault.main[0].identity[0].principal_id
 
   lifecycle {
     create_before_destroy = true
@@ -33,9 +46,9 @@ resource "azurerm_data_protection_backup_instance_postgresql_flexible_server" "m
   name     = "${local.server_name}-backup-instance"
   location = local.postgresql_rg_location
 
-  vault_id         = var.backup_vault_id
+  vault_id         = data.azurerm_data_protection_backup_vault.main[0].id
   server_id        = azurerm_postgresql_flexible_server.pgsql_server.id
-  backup_policy_id = var.backup_policy_id
+  backup_policy_id = data.azurerm_data_protection_backup_policy_postgresql_flexible_server.main[0].id
 
   depends_on = [
     azurerm_role_assignment.backup_vault_postgres_ltr,
