60a8ff30481ded5c218bab78d17e4af185cfec76
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2026-05-28 14:41:42 +0100

    revert to var

--- pgsql-flexible-server.tf
+++ pgsql-flexible-server.tf
@@ -60,7 +60,7 @@ data "azuread_group" "db_report_admin" {
 
 data "azuread_service_principal" "ptl_jenkins" {
   count     = var.enable_read_only_group_access ? 1 : 0
-  object_id = local.ptl_jenkins_object_id
+  object_id = var.admin_user_object_id
 }
 
 data "azuread_service_principal" "env_jenkins" {
